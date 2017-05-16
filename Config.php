9adfc8b52341a1f28d728c840fee0aa452182d10
airomad <airomad7@gmail.com>
2017-05-16 14:17:58 +0300

    Added all worked files / Cleaned Config.php file

---
+++ Config.php
@@ -0,0 +1,13 @@
+<?php
+
+    define("ORDER_MAIL_USERNAME", "~~~INSERT_HERE~~~");
+    define("ORDER_MAIL_HOST", "~~~INSERT_HERE~~~");
+    define("ORDER_MAIL_PORT", "~~~INSERT_HERE~~~");
+    define("ORDER_MAIL_PASSWORD", "~~~INSERT_HERE~~~");
+    define("ORDER_MAIL_CHARSET", "utf-8");
+    define("ORDER_MAIL_SENDER", "~~~INSERT_HERE~~~");
+
+    define("ORDER_MAIL_DEFAULT_TITLE", "Order on the Website");
+    define("ORDER_MAIL_EMPTY_FIELD", "EMPTY FIELD");
+    define("ORDER_MAIL_EMPTY_VALUE", "EMPTY VALUE");
+?>
